cb58357b9830bf0ae2a2329e370ac0a6342a3397
Carl Spain <neoancient@megamek.org>
2019-12-23 21:02:38 -0600

    Moved vee hit location table code to a separate class.
    This allows easy access by both the tables half-sheet and the full-page naval sheet.

---
+++ src/main/kotlin/org/megamek/rstemplate/layout/VeeHitLocationTable.kt
@@ -0,0 +1,163 @@
+package org.megamek.rstemplate.layout
+
+import org.apache.batik.util.SVGConstants
+import org.megamek.rstemplate.templates.FONT_SIZE_VSMALL
+import org.megamek.rstemplate.templates.RecordSheet
+import org.megamek.rstemplate.templates.truncate
+import org.w3c.dom.Element
+import java.util.*
+
+/**
+ *
+ */
+abstract class VeeHitLocationTable(private val sheet: RecordSheet) {
+
+    abstract val unitType: String
+    abstract val hitLocNotesCount: Int
+    abstract val hitLocations: List<List<String>>
+
+    protected val bundle: ResourceBundle = ResourceBundle.getBundle(VeeHitLocationTable::class.java.name)
+
+    abstract fun title(): String
+
+    fun draw(rect: Cell, parent: Element = sheet.document.documentElement) {
+        val g = sheet.createTranslatedGroup(rect.x, rect.y)
+        val inner = sheet.addBorder(0.0, 0.0, rect.width, rect.height,
+            title(), topTab = false, bottomTab = false,
+            parent = g)
+        val lineHeight = sheet.calcFontHeight(FONT_SIZE_VSMALL)
+        var ypos = inner.y + lineHeight
+        sheet.addTextElement(inner.width * 0.55, ypos, bundle.getString("attackDirection"),
+            FONT_SIZE_VSMALL, SVGConstants.SVG_BOLD_VALUE, anchor = SVGConstants.SVG_MIDDLE_VALUE,
+            parent = g)
+        ypos += lineHeight
+        ypos += sheet.createTable(0.0, ypos, inner.width, FONT_SIZE_VSMALL,
+            hitLocations, listOf(0.1, 0.3, 0.55, 0.8),
+            listOf(bundle.getString("2d6Roll"),
+                bundle.getString("dirFront"),
+                bundle.getString("dirRear"),
+                bundle.getString("dirSides")), parent = g)
+        ypos += lineHeight
+
+        val notesG = sheet.document.createElementNS(null, SVGConstants.SVG_G_TAG)
+        var notesY = 0.0
+        for (i in 1..hitLocNotesCount) {
+            notesY += sheet.addParagraph(
+                org.megamek.rstemplate.templates.padding, notesY, inner.width - org.megamek.rstemplate.templates.padding * 2, bundle.getString("hitLocNotes.$unitType.$i"),
+                FONT_SIZE_VSMALL, notesG)
+        }
+        notesG.setAttributeNS(null, SVGConstants.SVG_TRANSFORM_ATTRIBUTE,
+            "${SVGConstants.SVG_TRANSLATE_VALUE}(0,${(inner.bottomY() - notesY - org.megamek.rstemplate.templates.padding -lineHeight).truncate()})")
+        g.appendChild(notesG)
+        parent.appendChild(g)
+    }
+}
+
+open class TankHitLocationTable(sheet: RecordSheet): VeeHitLocationTable(sheet) {
+    override val unitType = "tank"
+    override val hitLocNotesCount = 4
+    override val hitLocations = listOf(
+        listOf("2*",
+            bundle.getString("front") + bundle.getString("critical"),
+            bundle.getString("rear") + bundle.getString("critical"),
+            bundle.getString("side") + bundle.getString("critical")),
+        listOf("3",
+            bundle.getString("front") + "\u2020",
+            bundle.getString("rear") + "\u2020",
+            bundle.getString("side") + "\u2020"),
+        listOf("4",
+            bundle.getString("front") + "\u2020",
+            bundle.getString("rear") + "\u2020",
+            bundle.getString("side") + "\u2020"),
+        listOf("5",
+            bundle.getString("rightSide") + "\u2020",
+            bundle.getString("leftSide") + "\u2020",
+            bundle.getString("front") + "\u2020"),
+        listOf("6",
+            bundle.getString("front"),
+            bundle.getString("rear"),
+            bundle.getString("side")),
+        listOf("7",
+            bundle.getString("front"),
+            bundle.getString("rear"),
+            bundle.getString("side")),
+        listOf("8",
+            bundle.getString("front"),
+            bundle.getString("rear"),
+            bundle.getString("side") + bundle.getString("critical") + "*"),
+        listOf("9",
+            bundle.getString("leftSide") + "\u2020",
+            bundle.getString("rightSide") + "\u2020",
+            bundle.getString("rear") + "\u2020"),
+        listOf("10",
+            bundle.getString("turret"),
+            bundle.getString("turret"),
+            bundle.getString("turret")),
+        listOf("11",
+            bundle.getString("turret"),
+            bundle.getString("turret"),
+            bundle.getString("turret")),
+        listOf("12*",
+            bundle.getString("turret") + bundle.getString("critical"),
+            bundle.getString("turret") + bundle.getString("critical"),
+            bundle.getString("turret") + bundle.getString("critical"))
+    )
+
+    override fun title(): String = bundle.getString("hitTable.tank.title")
+}
+
+class NavalHitLocationTable(sheet: RecordSheet): TankHitLocationTable(sheet) {
+    override fun title(): String = bundle.getString("hitTable.naval.title")
+}
+
+class VTOLHitLocationTable(sheet: RecordSheet): VeeHitLocationTable(sheet) {
+    override val unitType = "vtol"
+    override val hitLocNotesCount = 3
+    override val hitLocations = listOf(
+        listOf("2*",
+            bundle.getString("front") + bundle.getString("critical"),
+            bundle.getString("rear") + bundle.getString("critical"),
+            bundle.getString("side") + bundle.getString("critical")),
+        listOf("3",
+            bundle.getString("rotors") + "\u2020",
+            bundle.getString("rotors") + "\u2020",
+            bundle.getString("rotors") + "\u2020"),
+        listOf("4",
+            bundle.getString("turret") + "\u2021",
+            bundle.getString("turret") + "\u2021",
+            bundle.getString("turret") + "\u2021"),
+        listOf("5",
+            bundle.getString("rightSide") + "\u2020",
+            bundle.getString("leftSide") + "\u2020",
+            bundle.getString("front") + "\u2020"),
+        listOf("6",
+            bundle.getString("front"),
+            bundle.getString("rear"),
+            bundle.getString("side")),
+        listOf("7",
+            bundle.getString("front"),
+            bundle.getString("rear"),
+            bundle.getString("side")),
+        listOf("8",
+            bundle.getString("front"),
+            bundle.getString("rear"),
+            bundle.getString("side") + bundle.getString("critical") + "*"),
+        listOf("9",
+            bundle.getString("leftSide"),
+            bundle.getString("rightSide"),
+            bundle.getString("rear")),
+        listOf("10",
+            bundle.getString("rotors") + "\u2020",
+            bundle.getString("rotors") + "\u2020",
+            bundle.getString("rotors") + "\u2020"),
+        listOf("11",
+            bundle.getString("rotors") + "\u2020",
+            bundle.getString("rotors") + "\u2020",
+            bundle.getString("rotors") + "\u2020"),
+        listOf("12*",
+            bundle.getString("rotors") + bundle.getString("critical") + "*\u2020",
+            bundle.getString("rotors") + bundle.getString("critical") + "*\u2020",
+            bundle.getString("rotors") + bundle.getString("critical") + "*\u2020"))
+
+    override fun title(): String = bundle.getString("hitTable.vtol.title")
+}
--- src/main/kotlin/org/megamek/rstemplate/templates/VehicleTables.kt
+++ src/main/kotlin/org/megamek/rstemplate/templates/VehicleTables.kt
@@ -1,11 +1,8 @@
 package org.megamek.rstemplate.templates
 
 import org.apache.batik.util.SVGConstants
-import org.megamek.rstemplate.layout.Cell
-import org.megamek.rstemplate.layout.PaperSize
-import org.megamek.rstemplate.layout.RoundedBorder
+import org.megamek.rstemplate.layout.*
 import org.w3c.dom.Element
-import java.lang.Integer.max
 import java.util.*
 
 /**
@@ -27,48 +24,15 @@ abstract class VehicleTables(size: PaperSize): RecordSheet(size) {
     protected val bundle = ResourceBundle.getBundle(VehicleTables::class.java.name)
 
     abstract val unitType: String
-    abstract val hitLocNotesCount: Int
+    abstract val hitLocationTable: VeeHitLocationTable
     abstract val critTable: List<List<String>>
     abstract val critTableOffsets: List<Double>
     abstract val critTableLocations: List<String>
 
     override fun build() {
-        addHitLocationTable(hitLocation)
+         hitLocationTable.draw(hitLocation)
         addMotiveDamageTable(motiveDamage)
         addCriticalHitTable(criticalHits)
-    }
-
-    abstract val hitLocations: List<List<String>>
-
-    fun addHitLocationTable(rect: Cell) {
-        val g = createTranslatedGroup(rect.x, rect.y)
-        val inner = addBorder(0.0, 0.0, rect.width, rect.height,
-            bundle.getString("hitTable.$unitType.title"), false, false,
-            parent = g)
-        val lineHeight = calcFontHeight(FONT_SIZE_VSMALL)
-        var ypos = inner.y + lineHeight
-        addTextElement(inner.width * 0.55, ypos, bundle.getString("attackDirection"),
-            FONT_SIZE_VSMALL, SVGConstants.SVG_BOLD_VALUE, anchor = SVGConstants.SVG_MIDDLE_VALUE,
-            parent = g)
-        ypos += lineHeight
-        ypos += createTable(0.0, ypos, inner.width, FONT_SIZE_VSMALL,
-            hitLocations, listOf(0.1, 0.3, 0.55, 0.8),
-            listOf(bundle.getString("2d6Roll"),
-                bundle.getString("dirFront"),
-                bundle.getString("dirRear"),
-                bundle.getString("dirSides")), parent = g)
-        ypos += lineHeight
-
-        val notesG = document.createElementNS(null, SVGConstants.SVG_G_TAG)
-        var notesY = 0.0
-        for (i in 1..hitLocNotesCount) {
-            notesY += addParagraph(padding, notesY, inner.width - padding * 2, bundle.getString("hitLocNotes.$unitType.$i"),
-                FONT_SIZE_VSMALL, notesG)
-        }
-        notesG.setAttributeNS(null, SVGConstants.SVG_TRANSFORM_ATTRIBUTE,
-            "${SVGConstants.SVG_TRANSLATE_VALUE}(0,${(inner.bottomY() - notesY - padding -lineHeight).truncate()})")
-        g.appendChild(notesG)
-        document.documentElement.appendChild(g)
     }
 
     open fun addMotiveDamageTable(rect: Cell) {
@@ -132,53 +96,7 @@ abstract class VehicleTables(size: PaperSize): RecordSheet(size) {
 class TankTables(size: PaperSize): VehicleTables(size) {
     override val fileName = "tables_tank.svg"
     override val unitType = "tank"
-    override val hitLocNotesCount = 4
-    override val hitLocations = listOf(
-        listOf("2*",
-            bundle.getString("front") + bundle.getString("critical"),
-            bundle.getString("rear") + bundle.getString("critical"),
-            bundle.getString("side") + bundle.getString("critical")),
-        listOf("3",
-            bundle.getString("front") + "\u2020",
-            bundle.getString("rear") + "\u2020",
-            bundle.getString("side") + "\u2020"),
-        listOf("4",
-            bundle.getString("front") + "\u2020",
-            bundle.getString("rear") + "\u2020",
-            bundle.getString("side") + "\u2020"),
-        listOf("5",
-            bundle.getString("rightSide") + "\u2020",
-            bundle.getString("leftSide") + "\u2020",
-            bundle.getString("front") + "\u2020"),
-        listOf("6",
-            bundle.getString("front"),
-            bundle.getString("rear"),
-            bundle.getString("side")),
-        listOf("7",
-            bundle.getString("front"),
-            bundle.getString("rear"),
-            bundle.getString("side")),
-        listOf("8",
-            bundle.getString("front"),
-            bundle.getString("rear"),
-            bundle.getString("side") + bundle.getString("critical") + "*"),
-        listOf("9",
-            bundle.getString("leftSide") + "\u2020",
-            bundle.getString("rightSide") + "\u2020",
-            bundle.getString("rear") + "\u2020"),
-        listOf("10",
-            bundle.getString("turret"),
-            bundle.getString("turret"),
-            bundle.getString("turret")),
-        listOf("11",
-            bundle.getString("turret"),
-            bundle.getString("turret"),
-            bundle.getString("turret")),
-        listOf("12*",
-            bundle.getString("turret") + bundle.getString("critical"),
-            bundle.getString("turret") + bundle.getString("critical"),
-            bundle.getString("turret") + bundle.getString("critical"))
-    )
+    override val hitLocationTable = TankHitLocationTable(this)
 
     override val critTableOffsets = listOf(0.15, 0.27, 0.45, 0.63, 0.81)
     override val critTableLocations = listOf(bundle.getString("2d6Roll"), bundle.getString("front"),
@@ -216,52 +134,7 @@ class VTOLTables(size: PaperSize): VehicleTables(size) {
 
     override val fileName = "tables_vtol.svg"
     override val unitType = "vtol"
-    override val hitLocNotesCount = 3
-    override val hitLocations = listOf(
-        listOf("2*",
-            bundle.getString("front") + bundle.getString("critical"),
-            bundle.getString("rear") + bundle.getString("critical"),
-            bundle.getString("side") + bundle.getString("critical")),
-        listOf("3",
-            bundle.getString("rotors") + "\u2020",
-            bundle.getString("rotors") + "\u2020",
-            bundle.getString("rotors") + "\u2020"),
-        listOf("4",
-            bundle.getString("turret") + "\u2021",
-            bundle.getString("turret") + "\u2021",
-            bundle.getString("turret") + "\u2021"),
-        listOf("5",
-            bundle.getString("rightSide") + "\u2020",
-            bundle.getString("leftSide") + "\u2020",
-            bundle.getString("front") + "\u2020"),
-        listOf("6",
-            bundle.getString("front"),
-            bundle.getString("rear"),
-            bundle.getString("side")),
-        listOf("7",
-            bundle.getString("front"),
-            bundle.getString("rear"),
-            bundle.getString("side")),
-        listOf("8",
-            bundle.getString("front"),
-            bundle.getString("rear"),
-            bundle.getString("side") + bundle.getString("critical") + "*"),
-        listOf("9",
-            bundle.getString("leftSide"),
-            bundle.getString("rightSide"),
-            bundle.getString("rear")),
-        listOf("10",
-            bundle.getString("rotors") + "\u2020",
-            bundle.getString("rotors") + "\u2020",
-            bundle.getString("rotors") + "\u2020"),
-        listOf("11",
-            bundle.getString("rotors") + "\u2020",
-            bundle.getString("rotors") + "\u2020",
-            bundle.getString("rotors") + "\u2020"),
-        listOf("12*",
-            bundle.getString("rotors") + bundle.getString("critical") + "*\u2020",
-            bundle.getString("rotors") + bundle.getString("critical") + "*\u2020",
-            bundle.getString("rotors") + bundle.getString("critical") + "*\u2020"))
+    override val hitLocationTable = VTOLHitLocationTable(this)
 
     override val critTableOffsets = listOf(0.1, 0.21, 0.35, 0.53, 0.71, 0.85)
     override val critTableLocations = listOf(bundle.getString("2d6Roll"), bundle.getString("front"),
