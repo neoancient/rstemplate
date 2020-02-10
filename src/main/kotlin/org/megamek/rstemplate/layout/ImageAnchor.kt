1f716d4a99a51700421772ad350a98a85603db1a
Carl Spain <neoancient@megamek.org>
2020-02-09 21:16:35 -0600

    Warnings cleanup

--- src/main/kotlin/org/megamek/rstemplate/layout/ImageAnchor.kt
+++ src/main/kotlin/org/megamek/rstemplate/layout/ImageAnchor.kt
@@ -5,13 +5,13 @@ package org.megamek.rstemplate.layout
  * height of the area to place the image in and the width or height of the image.
  */
 enum class ImageAnchor(val xOffset: (Double, Double) -> Double, val yOffset: (Double, Double) -> Double) {
-    TOP_LEFT ({areaW, imageW -> 0.0}, {areaH, imageH -> 0.0}),
-    TOP ({areaW, imageW -> (areaW - imageW) * 0.5}, {areaH, imageH -> 0.0}),
-    TOP_RIGHT ({areaW, imageW -> areaW - imageW}, {areaH, imageH -> 0.0}),
-    LEFT ({areaW, imageW -> 0.0}, {areaH, imageH -> (areaH - imageH) * 0.5}),
+    TOP_LEFT ({_, _ -> 0.0}, {_, _ -> 0.0}),
+    TOP ({areaW, imageW -> (areaW - imageW) * 0.5}, {_, _ -> 0.0}),
+    TOP_RIGHT ({areaW, imageW -> areaW - imageW}, {_, _ -> 0.0}),
+    LEFT ({_, _ -> 0.0}, {areaH, imageH -> (areaH - imageH) * 0.5}),
     CENTER ({areaW, imageW -> (areaW - imageW) * 0.5}, {areaH, imageH -> (areaH - imageH) * 0.5}),
     RIGHT ({areaW, imageW -> areaW - imageW}, {areaH, imageH -> (areaH - imageH) * 0.5}),
-    BOTTOM_LEFT ({areaW, imageW -> 0.0}, {areaH, imageH -> areaH - imageH}),
+    BOTTOM_LEFT ({_, _ -> 0.0}, {areaH, imageH -> areaH - imageH}),
     BOTTOM ({areaW, imageW -> (areaW - imageW) * 0.5}, {areaH, imageH -> areaH - imageH}),
     BOTTOM_RIGHT ({areaW, imageW -> areaW - imageW}, {areaH, imageH -> areaH - imageH})
 }
